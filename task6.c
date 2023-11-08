2c70f7d28b52e9ab2353635452a37b9a3d55e9ea
Shahriar Najim <139145898+shahriarn10@users.noreply.github.com>
2023-11-08 20:41:46 +0600

    task6.c

--- task6.c
+++ task6.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int countOccurrence(int arr[],int size,int target)
 {
-    int count =0;
+    int count=0;
     for(int i=0; i<size; i++)
     {
         if(arr[i]==target)
@@ -15,10 +15,10 @@ int main()
 {
     int arr[]= {5,2,4,6,3,4,7,5,5};
     int size=sizeof(arr)/sizeof(arr[0]);
-    int target;
+    int find;
     printf("Enter the integer to count:");
-    scanf("%d",&target);
-    int occurrence=countOccurrence(arr,size,target);
-    printf("The integer %d appears in the array %d times.\n",target,occurrence);
+    scanf("%d",&find);
+    int occurrence=countOccurrence(arr,size,find);
+    printf("The integer %d appears in the array %d times.\n",find,occurrence);
     return 0;
 }
