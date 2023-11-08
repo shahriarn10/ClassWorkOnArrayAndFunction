c35bbdf723dd293880ba2e2ae2dfde983efee1d4
Shahriar Najim <139145898+shahriarn10@users.noreply.github.com>
2023-11-08 20:25:04 +0600

    task6.c

--- task6.c
+++ task6.c
@@ -13,7 +13,7 @@ int countOccurrence(int arr[],int size,int target)
 }
 int main()
 {
-    int arr[]= {7,2,3,4,3,6,5,7,7};
+    int arr[]= {5,2,4,6,3,4,7,5,5};
     int size=sizeof(arr)/sizeof(arr[0]);
     int target;
     printf("Enter the integer to count:");
